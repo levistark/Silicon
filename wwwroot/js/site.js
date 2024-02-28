f743701d04696321451b95f5874871dc632a9caf
Levi Stark <levi.john.stark@outlook.com>
2024-02-28 14:59:22 +0100

    enabled dark and light-mode toggling

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,22 @@
 ﻿
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('lastBodyClass') === 'dark') {
+        document.body.classList.add('dark')
+        document.querySelector('#switch-mode').checked = true;
+    }
+    else {
+        document.body.classList.remove('dark')
+        document.querySelector('#switch-mode').checked = false;
+    }
+})
+
+document.querySelector('#switch-mode').addEventListener('click', (e) => {
+    if (document.body.classList.contains('dark')) 
+        document.body.classList.remove('dark')
+    else 
+        document.body.classList.add('dark')
+    
+    localStorage.setItem('lastBodyClass', document.body.classList[0])
+})
+
+
