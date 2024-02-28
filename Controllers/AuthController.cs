f7e11d3e131a6c61911ce5dcb93e25fd923117cd
Levi Stark <levi.john.stark@outlook.com>
2024-02-28 16:02:24 +0100

    made some progress with the flyout-navbar

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon.Controllers;
+public class AuthController : Controller
+{
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,8 @@
 ﻿
+/* 
+This function handles the load of the user's last theme setting.
+It uses localStorage to load the last saved color class (lastBodyClass) from the body-tag
+*/
 document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('lastBodyClass') === 'dark') {
         document.body.classList.add('dark')
@@ -10,6 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
+/*
+This function toggles between dark and light mode on click at the round switch button on the header.
+It adds/removes a predefined class name 'dark' to the body-tags classlist. 
+Then it saves the user's last setting to localStorage. 
+*/
 document.querySelector('#switch-mode').addEventListener('click', (e) => {
     if (document.body.classList.contains('dark')) 
         document.body.classList.remove('dark')
@@ -18,5 +27,20 @@ document.querySelector('#switch-mode').addEventListener('click', (e) => {
     
     localStorage.setItem('lastBodyClass', document.body.classList[0])
 })
+
+/*
+This function hides/shows the the flyout mobile navigation bar.
+*/
+function toggleNavBar() {
+
+    let sidebar = document.querySelector('.mobile-nav-wrapper')
+    console.log(1)
+
+    if (sidebar.classList.contains('d-none')) {
+        sidebar.classList.remove('d-none')
+    } else {
+        sidebar.classList.add('d-none')
+    }
+}
 
 
